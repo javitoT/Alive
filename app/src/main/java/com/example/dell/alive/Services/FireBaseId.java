a28f7e5e7f3a579ba6cc9e07e39dfba7454cff8d
javitoT <31636765+javitoT@users.noreply.github.com>
2017-09-25 13:41:55 -0500

    Starting to set Firebase into the code

---
+++ app/src/main/java/com/example/dell/alive/Services/FireBaseId.java
@@ -0,0 +1,16 @@
+package com.example.dell.alive.Services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by DELL on 22/09/2017.
+ */
+
+public class FireBaseId extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+    }
+}
---
+++ app/src/main/java/com/example/dell/alive/Services/FireBaseServiceMensajes.java
@@ -0,0 +1,15 @@
+package com.example.dell.alive.Services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by DELL on 22/09/2017.
+ */
+
+public class FireBaseServiceMensajes extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
